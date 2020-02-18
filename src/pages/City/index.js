0ce25ac0bdb2c13be553c50af41f346bfb83671d
flows1314 <1778238738@qq.com>
2020-02-18 22:43:07 +0800

    编辑模态框

--- src/pages/City/index.js
+++ src/pages/City/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './index.less'
 import axios from './../../axios'
-import { Card, Form, Input, Select, Button, Table } from 'antd'
+import { Card, Form, Select, Button, Table, Modal, Radio } from 'antd'
 class City extends React.Component {
   state = {}
   componentDidMount() {
     this.requestList()
+  }
+  handleSubmit = () => {
+    this.setState({
+      showOpen: true
+    })
   }
   requestList = () => {
     axios.ajax({
@@ -27,6 +32,15 @@ class City extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     const { Option } = Select;
+    const formItemLayout={
+      labelCol:{
+        span:4
+      }, 
+      wrapperCol:{
+        span:15,
+        offset:1
+      }
+    }
     const columns = [
       {
         title: '城市ID',
@@ -77,8 +91,7 @@ class City extends React.Component {
         title: '城市开通时间',
         dataIndex: 'openTime',
         key: 'openTime',
-      },
-
+      }
     ];
     return (
       <div>
@@ -136,13 +149,50 @@ class City extends React.Component {
             </Form.Item>
           </Form>
         </Card>
-        <Card title={<Button type='primary'>开通城市</Button>}>
+        <Card title={<Button type='primary' onClick={this.handleSubmit}>开通城市</Button>}>
           <Table
             columns={columns}
             dataSource={this.state.list}
             bordered
           />
         </Card>
+        <Modal 
+          title='开通城市' 
+          visible={this.state.showOpen}
+          onCancel={()=>{this.setState({showOpen:false})}}
+          >
+          <Form>
+            <Form.Item label='选择城市' {...formItemLayout} >
+              {getFieldDecorator('selectCity', {
+                initialValue: '1'
+              })(<Select style={{width:120}}>
+                <Option value='1'>北京</Option>
+                <Option value='2'>上海</Option>
+                <Option value='3'>深圳</Option>
+              </Select>)}
+            </Form.Item>
+            <Form.Item label='营运模式' {...formItemLayout}>
+              {getFieldDecorator('operatingMode', {
+                initialValue: '1'
+              })(
+                <Radio.Group>
+                  <Radio value='1'>自营</Radio>
+                  <Radio value='2'>加盟</Radio>
+                </Radio.Group>
+              )}
+            </Form.Item>
+            <Form.Item label='用车模式' {...formItemLayout}>
+              {getFieldDecorator('vehicleMode', {
+                initialValue: '1'
+              })(
+                <Radio.Group>
+                  <Radio value='1'>指定停车点模式</Radio>
+                  <Radio value='2'>禁停区模式</Radio>
+                </Radio.Group>
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     )
   }
