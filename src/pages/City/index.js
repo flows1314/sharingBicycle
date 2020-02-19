458421bf5471f19c3e42d4883bd404e9568bb841
flows1314 <1778238738@qq.com>
2020-02-19 15:46:19 +0800

    城市管理完成

--- src/pages/City/index.js
+++ src/pages/City/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import './index.less'
 import axios from './../../axios'
-import { Card, Form, Select, Button, Table, Modal, Radio } from 'antd'
+import { Card, Form, Select, Button, Table, Modal, Radio, message } from 'antd'
 class City extends React.Component {
   state = {}
   componentDidMount() {
     this.requestList()
   }
   handleSubmit = () => {
+    let cityInfo = this.cityForm.props.form.getFieldsValue();
+    axios.ajax({
+      url: 'form/city_submit',
+      data: {
+        params: cityInfo
+      }
+    }).then((res) => {
+      if (res.code == 0) {
+        message.success(res.result);
+        this.setState({ showOpen: false });
+        this.requestList()
+      }
+    })
+  }
+  handleOpen = () => {
     this.setState({
       showOpen: true
     })
@@ -30,17 +45,6 @@ class City extends React.Component {
     })
   }
   render() {
-    const { getFieldDecorator } = this.props.form;
-    const { Option } = Select;
-    const formItemLayout={
-      labelCol:{
-        span:4
-      }, 
-      wrapperCol:{
-        span:15,
-        offset:1
-      }
-    }
     const columns = [
       {
         title: '城市ID',
@@ -56,11 +60,17 @@ class City extends React.Component {
         title: '用车模式',
         dataIndex: 'carModal',
         key: 'carModal',
+        render(carModal) {
+          return carModal == '1' ? '停车点' : '禁停区';
+        }
       },
       {
         title: '营运模式',
         dataIndex: 'OperatingModel',
         key: 'OperatingModel',
+        render(OperatingModel) {
+          return OperatingModel == '1' ? '自营' : '加盟';
+        }
       },
       {
         title: '授权加盟商',
@@ -95,106 +105,140 @@ class City extends React.Component {
     ];
     return (
       <div>
-        <Card className='card-wrap'>
-          <Form layout="inline">
-            <Form.Item label='城市'>
-              {getFieldDecorator('city', {
-                initialValue: '全部'
-              })(
-                <Select style={{ width: 90 }}>
-                  <Option value='1'>北京</Option>
-                  <Option value='2'>上海</Option>
-                  <Option value='3'>广州</Option>
-                  <Option value='4'>深圳</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item label='用车模式'>
-              {getFieldDecorator('carModel', {
-                initialValue: '全部'
-              })(
-                <Select style={{ width: 150 }}>
-                  <Option value='1'>自营</Option>
-                  <Option value='2'>出租</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item label='营运模式'>
-              {getFieldDecorator('OperatingModel', {
-                initialValue: '全部'
-              })(
-                <Select style={{ width: 80 }}>
-                  <Option value='1'>北京</Option>
-                  <Option value='2'>上海</Option>
-                  <Option value='3'>广州</Option>
-                  <Option value='4'>深圳</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item label='加盟商授权状态'>
-              {getFieldDecorator('state', {
-                initialValue: '全部'
-              })(
-                <Select style={{ width: 80 }}>
-                  <Option value='1'>北京</Option>
-                  <Option value='2'>上海</Option>
-                  <Option value='3'>广州</Option>
-                  <Option value='4'>深圳</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              <Button type='primary' style={{ margin: '0 20px 0 30px' }}>查询</Button>
-              <Button>重置</Button>
-            </Form.Item>
-          </Form>
-        </Card>
-        <Card title={<Button type='primary' onClick={this.handleSubmit}>开通城市</Button>}>
+        <FuncComponent />
+        <Card title={<Button type='primary' onClick={this.handleOpen}>开通城市</Button>}>
           <Table
             columns={columns}
             dataSource={this.state.list}
             bordered
           />
         </Card>
-        <Modal 
-          title='开通城市' 
+        <Modal
+          title='开通城市'
           visible={this.state.showOpen}
-          onCancel={()=>{this.setState({showOpen:false})}}
-          >
-          <Form>
-            <Form.Item label='选择城市' {...formItemLayout} >
-              {getFieldDecorator('selectCity', {
-                initialValue: '1'
-              })(<Select style={{width:120}}>
-                <Option value='1'>北京</Option>
-                <Option value='2'>上海</Option>
-                <Option value='3'>深圳</Option>
-              </Select>)}
-            </Form.Item>
-            <Form.Item label='营运模式' {...formItemLayout}>
-              {getFieldDecorator('operatingMode', {
-                initialValue: '1'
-              })(
-                <Radio.Group>
-                  <Radio value='1'>自营</Radio>
-                  <Radio value='2'>加盟</Radio>
-                </Radio.Group>
-              )}
-            </Form.Item>
-            <Form.Item label='用车模式' {...formItemLayout}>
-              {getFieldDecorator('vehicleMode', {
-                initialValue: '1'
-              })(
-                <Radio.Group>
-                  <Radio value='1'>指定停车点模式</Radio>
-                  <Radio value='2'>禁停区模式</Radio>
-                </Radio.Group>
-              )}
-            </Form.Item>
-          </Form>
+          onCancel={() => { this.setState({ showOpen: false }) }}
+          onOk={this.handleSubmit}
+        >
+          <ModalComponent wrappedComponentRef={(inst) => { this.cityForm = inst; }} />
         </Modal>
       </div>
     )
   }
 }
-export default Form.create()(City);
+export default City;
+
+//开通城市弹框组件
+class carModal extends React.Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { Option } = Select;
+    const formItemLayout = {
+      labelCol: {
+        span: 4
+      },
+      wrapperCol: {
+        span: 15,
+        offset: 1
+      }
+    }
+    return (
+      <Form>
+        <Form.Item label='选择城市' {...formItemLayout} >
+          {getFieldDecorator('selectCity', {
+            initialValue: '1'
+          })(<Select style={{ width: 120 }}>
+            <Option value='1'>北京</Option>
+            <Option value='2'>上海</Option>
+            <Option value='3'>深圳</Option>
+          </Select>)}
+        </Form.Item>
+        <Form.Item label='营运模式' {...formItemLayout}>
+          {getFieldDecorator('operatingMode', {
+            initialValue: '1'
+          })(
+            <Radio.Group>
+              <Radio value='1'>自营</Radio>
+              <Radio value='2'>加盟</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item>
+        <Form.Item label='用车模式' {...formItemLayout}>
+          {getFieldDecorator('vehicleMode', {
+            initialValue: '1'
+          })(
+            <Radio.Group>
+              <Radio value='1'>指定停车点模式</Radio>
+              <Radio value='2'>禁停区模式</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item>
+      </Form>
+    )
+  }
+}
+const ModalComponent = Form.create()(carModal)
+
+//头部查询重置等组件
+class Func extends React.Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { Option } = Select;
+    return (
+      <Card className='card-wrap'>
+        <Form layout="inline">
+          <Form.Item label='城市'>
+            {getFieldDecorator('city', {
+              initialValue: '全部'
+            })(
+              <Select style={{ width: 90 }}>
+                <Option value='1'>北京</Option>
+                <Option value='2'>上海</Option>
+                <Option value='3'>广州</Option>
+                <Option value='4'>深圳</Option>
+              </Select>
+            )}
+          </Form.Item>
+          <Form.Item label='用车模式'>
+            {getFieldDecorator('carModel', {
+              initialValue: '全部'
+            })(
+              <Select style={{ width: 150 }}>
+                <Option value='1'>自营</Option>
+                <Option value='2'>出租</Option>
+              </Select>
+            )}
+          </Form.Item>
+          <Form.Item label='营运模式'>
+            {getFieldDecorator('OperatingModel', {
+              initialValue: '全部'
+            })(
+              <Select style={{ width: 80 }}>
+                <Option value='1'>北京</Option>
+                <Option value='2'>上海</Option>
+                <Option value='3'>广州</Option>
+                <Option value='4'>深圳</Option>
+              </Select>
+            )}
+          </Form.Item>
+          <Form.Item label='加盟商授权状态'>
+            {getFieldDecorator('state', {
+              initialValue: '全部'
+            })(
+              <Select style={{ width: 80 }}>
+                <Option value='1'>北京</Option>
+                <Option value='2'>上海</Option>
+                <Option value='3'>广州</Option>
+                <Option value='4'>深圳</Option>
+              </Select>
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' style={{ margin: '0 20px 0 30px' }}>查询</Button>
+            <Button>重置</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    )
+  }
+}
+const FuncComponent = Form.create()(Func)
