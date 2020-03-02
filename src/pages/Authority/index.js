e8a69cb106fc90d841a39ce0848906b22f2cf1ed
flows1314 <1778238738@qq.com>
2020-03-02 17:46:06 +0800

    权限meun

--- src/pages/Authority/index.js
+++ src/pages/Authority/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import axios from '../../axios'
 import utils from '../../utils/utils'
-import { Card, Button, Modal, Table, Form, Input, Select, message } from 'antd'
+import meun from '../../config/meunConfig'
+import { Card, Button, Modal, Table, Form, Input, Select, message, Tree } from 'antd'
 class Authority extends React.Component {
     state = { visible: false }
     componentDidMount() {
@@ -13,7 +14,8 @@ class Authority extends React.Component {
             .then((res) => {
                 if (res.code == 0) {
                     this.setState({
-                        list: res.result
+                        list: res.result,
+                        meunList: res.result[0].meun
                     })
                 }
             })
@@ -57,6 +59,40 @@ class Authority extends React.Component {
                     this.requestList()
                 }
             })
+    }
+    //权限设置
+    handleSetting = () => {
+        let item = this.state.selectedItem;
+        if (!item) {
+            Modal.info({
+                title: '权限设置提示',
+                content: '请先选择角色！'
+            })
+            return;
+        }
+        this.setState({
+            authority_visible: true,
+        })
+    }
+    //获取权限菜单
+    getCheckedKeys = (checkedKeys) => {
+        this.setState({
+            meunList: checkedKeys
+        })
+    }
+    //权限提交
+    handldAuthoritySubmit = () => {
+        let data = this.formAuthority.props.form.getFieldsValue();
+        data.user_id = this.state.selectedItem.id;
+        data.meunList = this.state.meunList;
+        axios.ajax({ url: 'authority/submit', data: { params: data } })
+            .then((res) => {
+                if (res.code == 0) {
+                    message.success(res.result);
+                    this.setState({ authority_visible: false, selectedRowKeys: '', selectedItem: '' })
+                }
+            })
+
     }
 
     render() {
@@ -110,8 +146,8 @@ class Authority extends React.Component {
         return (
             <div>
                 <Card>
-                    <Button type='primary' icon="plus" onClick={this.handleCreateRole}>创建角色</Button>
-                    <Button type="primary" icon="setting" style={{ margin: "0 3px" }}>设置权限</Button>
+                    <Button type='primary' onClick={this.handleCreateRole} icon="plus" >创建角色</Button>
+                    <Button type="primary" onClick={this.handleSetting} icon="setting" style={{ margin: "0 3px" }} >设置权限</Button>
                     <Button type='primary' icon="user">用户授权</Button>
                 </Card>
                 <Card>
@@ -130,10 +166,25 @@ class Authority extends React.Component {
                     onCancel={() => {
                         this.setState({ visible: false });
                         this.formItem.props.form.resetFields()
-                    }
-                    }
+                    }}
                 >
                     <FormRole wrappedComponentRef={(inst) => this.formItem = inst} />
+                </Modal>
+                <Modal
+                    title='设置权限'
+                    visible={this.state.authority_visible}
+                    onOk={this.handldAuthoritySubmit}
+                    onCancel={() => {
+                        this.setState({ authority_visible: false, selectedRowKeys: '', selectedItem: '' });
+                        this.formAuthority.props.form.resetFields()
+                    }}
+                >
+                    <FormAuthority
+                        wrappedComponentRef={(inst) => this.formAuthority = inst}
+                        item={this.state.selectedItem}
+                        meunList={this.state.meunList}
+                        getCheckedKeys={this.getCheckedKeys}
+                    />
                 </Modal>
             </div>
         )
@@ -170,3 +221,68 @@ class Role extends React.Component {
     }
 }
 const FormRole = Form.create()(Role)
+
+
+//权限设置组件
+class Author extends React.Component {
+    //遍历tree节点
+    renderTreeNodes = (meun) => {
+        const { TreeNode } = Tree;
+        return meun.map((item) => {
+            if (item.children) {
+                return <TreeNode title={item.title} key={item.key}>
+                    {this.renderTreeNodes(item.children)}
+                </TreeNode>
+            }
+            return <TreeNode title={item.title} key={item.key} />
+        })
+    }
+
+    onCheck = (checkedKeys) => {
+        this.props.getCheckedKeys(checkedKeys)
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const { roleName } = this.props.item;
+        const Option = Select.Option;
+        const { TreeNode } = Tree;
+        const meunList = this.props.meunList
+        const formLayout = {
+            labelCol: { span: 5 },
+            wrapperCol: { span: 14 }
+        }
+        const config = {
+            '1': '管理人员',
+            '2': '客服专员',
+            '3': '财务专员',
+            '4': '后勤人员',
+        }
+        return (
+            <Form layout='horizontal'>
+                <Form.Item label='角色名称' {...formLayout}>
+                    <Input disabled placeholder={config[roleName]} />
+                </Form.Item>
+                <Form.Item label='状态' {...formLayout}>
+                    {getFieldDecorator('state')(
+                        <Select placeholder='请选择当前状态'>
+                            <Option value={0}>开启</Option>
+                            <Option value={1}>关闭</Option>
+                        </Select>
+                    )}
+                </Form.Item>
+                <Tree
+                    checkable
+                    defaultExpandAll
+                    onCheck={(checkedKeys) => { this.onCheck(checkedKeys) }}
+                    checkedKeys={meunList}
+                >
+                    <TreeNode title='平台权限' key='plateAuthority'>
+                        {this.renderTreeNodes(meun)}
+                    </TreeNode>
+                </Tree>
+            </Form>
+        )
+    }
+}
+const FormAuthority = Form.create()(Author)
