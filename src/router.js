757462a015a6ff1e89b276210c18858bb72cd1cc
flows1314 <1778238738@qq.com>
2020-03-03 11:57:19 +0800

    路由调整

--- src/router.js
+++ src/router.js
@@ -27,47 +27,50 @@ import Pie from './pages/Echarts/Pie';
 import Line from './pages/Echarts/Line';
 import RichText from './pages/RichText';
 import Authority from './pages/Authority';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch,Redirect } from 'react-router-dom';
 class Router extends React.Component {
   render() {
     return (
       <HashRouter>
         <App>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/admin' render={
-            () => <Admin>
-              <Switch>
-                <Route path='/admin/home' component={Home}></Route>
-                <Route path='/admin/ui/buttons' component={Buttons}></Route>
-                <Route path='/admin/ui/loading' component={Loading}></Route>
-                <Route path='/admin/ui/modals' component={Modals}></Route>
-                <Route path='/admin/ui/notification' component={Notification}></Route>
-                <Route path='/admin/ui/messages' component={Message}></Route>
-                <Route path='/admin/ui/gallery' component={Gallery}></Route>
-                <Route path='/admin/ui/tabs' component={Tabs}></Route>
-                <Route path='/admin/ui/carousel' component={Carousel}></Route>
-                <Route path='/admin/form/log' component={Log}></Route>
-                <Route path='/admin/form/register' component={Register}></Route>
-                <Route path='/admin/table/basic' component={TableBasic}></Route>
-                <Route path='/admin/table/high' component={HighTable}></Route>
-                <Route path='/admin/city' component={City}></Route>
-                <Route path='/admin/order' component={Order}></Route>
-                <Route path='/admin/user' component={User}></Route>
-                <Route path='/admin/map' component={Map}></Route>
-                <Route path='/admin/echarts/bar' component={Bar}></Route>
-                <Route path='/admin/echarts/pie' component={Pie}></Route>
-                <Route path='/admin/echarts/line' component={Line}></Route>
-                <Route path='/admin/rich' component={RichText}></Route>
-                <Route path='/admin/authority' component={Authority}></Route>
-                <Route component={NoFound}></Route>
-              </Switch>
-            </Admin>}>
-          </Route>
-          <Route path='/common' render={() =>
-            <Common>
+          <Switch>
+            <Route path='/login' component={Login}></Route>
+            <Route path='/common' render={() =>
+              <Common>
                 <Route path='/common/order/detail/:orderID' component={Detail}></Route>
-            </Common>}>
-          </Route>
+              </Common>}>
+            </Route>
+            <Route path='/' render={
+              () => <Admin>
+                <Switch>
+                  <Route path='/home' component={Home}></Route>
+                  <Route path='/ui/buttons' component={Buttons}></Route>
+                  <Route path='/ui/loading' component={Loading}></Route>
+                  <Route path='/ui/modals' component={Modals}></Route>
+                  <Route path='/ui/notification' component={Notification}></Route>
+                  <Route path='/ui/messages' component={Message}></Route>
+                  <Route path='/ui/gallery' component={Gallery}></Route>
+                  <Route path='/ui/tabs' component={Tabs}></Route>
+                  <Route path='/ui/carousel' component={Carousel}></Route>
+                  <Route path='/form/log' component={Log}></Route>
+                  <Route path='/form/register' component={Register}></Route>
+                  <Route path='/table/basic' component={TableBasic}></Route>
+                  <Route path='/table/high' component={HighTable}></Route>
+                  <Route path='/city' component={City}></Route>
+                  <Route path='/order' component={Order}></Route>
+                  <Route path='/user' component={User}></Route>
+                  <Route path='/map' component={Map}></Route>
+                  <Route path='/echarts/bar' component={Bar}></Route>
+                  <Route path='/echarts/pie' component={Pie}></Route>
+                  <Route path='/echarts/line' component={Line}></Route>
+                  <Route path='/rich' component={RichText}></Route>
+                  <Route path='/authority' component={Authority}></Route>
+                  <Redirect to='/home'/>  //重定向放到最后，当匹配不到其他时在匹配它
+                  {/* <Route component={NoFound}></Route> */}
+                </Switch>
+              </Admin>}>
+            </Route>
+          </Switch>
         </App>
       </HashRouter>
     )
